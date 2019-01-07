cb1f12efb494f959c8325fd2889e7bd4a7b2496c
Mark Fitzpatrick <markfitz815@gmail.com>
2019-01-07 18:35:00 +0000

    set up initial index route

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.static(`${__dirname}/public`))
+
 app.listen(port, () => {
     console.log(`Running on port ${port}`);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/views/index.html`);
+})
